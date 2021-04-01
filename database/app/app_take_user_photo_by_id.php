ff7c86b0d8bdc3da6fe326b1c0574900e58e9f58
Lara Costa <laracostv@gmail.com>
2021-04-01 20:42:08 -0300

    get profile photo fix 2

--- database/app/app_take_user_photo_by_id.php
+++ database/app/app_take_user_photo_by_id.php
@@ -1,25 +1,24 @@
 <?php
     $link = '';
     // array que guarda a resposta da requisicao
-    $responseImg = array();
-    $responseImgArr = array();
+    $responseImgJson = array();
     
     require_once('../db.class.php');
     $objDb = new db();
     $link = $objDb->mysql_connect();
 
     if(isset($_GET['id_user'])){
+        $responseImg = array();
         $idUserSet = $_GET['id_user'];
 
         $search_image = mysqli_query($link, "SELECT usrProfilePhoto FROM users WHERE idUser = '$idUserSet'");
-        $responseImg["img"] = mysqli_fetch_array($search_image);
+        $searchImageSrc = mysqli_fetch_array($search_image);
+        $responseImg["img"] = $searchImageSrc[0];
         $responseImg["success"] = 1;
-
-        array_push($responseImgArr["userPhoto"], $responseImg);
     }else{
-        $responseImgArr["success"] = 0;
+        $responseImg["success"] = 0;
     }
 
     mysqli_close($link);
-    echo json_encode($responseImgArr);
+    echo json_encode($responseImg['img']);
 ?>
