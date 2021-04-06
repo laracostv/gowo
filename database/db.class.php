348fe040e9d5160720775a0fc0f5c4f5f3fe7d17
Lara Costa <laracostv@gmail.com>
2021-04-06 00:55:43 -0300

    newhost con1

--- database/db.class.php
+++ database/db.class.php
@@ -2,11 +2,21 @@
     class db {
         
         public function mysql_connect(){
-            
+            /*
+            //heroku
             $host = getenv('host_var');
             $usuario = getenv('user_var');
             $senha = getenv('pwd_var');
             $database = getenv('database_var');
+            */
+
+
+            //hostgator
+            $host = getenv('g_host_var');
+            $usuario = getenv('g_user_var');
+            $senha = getenv('g_pwd_var');
+            $database = getenv('g_database_var');
+
 
             $con = mysqli_connect($host, $usuario, $senha, $database);
 
