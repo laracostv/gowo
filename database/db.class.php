8c14fd59f23c00a0208875a2b4e69761b6fc3bc6
Lara Costa <laracostv@gmail.com>
2021-04-05 23:50:37 -0300

    var test 2

--- database/db.class.php
+++ database/db.class.php
@@ -1,18 +1,14 @@
 <?php
     class db {
-
-        //host
-        private $host = getenv('host_var');
-
-        //user
-        private $usuario = getenv('user_var');
-        private $senha = getenv('pwd_var');
-
-        //databade
-        private $database = getenv('database_var');
         
         public function mysql_connect(){
-            $con = mysqli_connect($this->host, $this->usuario, $this->senha, $this->database);
+            
+            $host = getenv('host_var');
+            $usuario = getenv('user_var');
+            $senha = getenv('pwd_var');
+            $database = getenv('pwd_var');
+
+            $con = mysqli_connect($host, $usuario, $senha, $database);
 
             mysqli_set_charset($con, 'utf8');
 
