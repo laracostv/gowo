54e0904d2a3103b7343c0c87dbafd082bf4f4402
Lara Costa <laracostv@gmail.com>
2021-04-05 23:55:51 -0300

    var test 3

--- database/db.class.php
+++ database/db.class.php
@@ -6,11 +6,15 @@
             $host = getenv('host_var');
             $usuario = getenv('user_var');
             $senha = getenv('pwd_var');
-            $database = getenv('pwd_var');
+            $database = getenv('database_var');
 
             $con = mysqli_connect($host, $usuario, $senha, $database);
 
             mysqli_set_charset($con, 'utf8');
+            
+            if ($con->connect_error) {
+                die("Connection failed: " . $con->connect_error);
+            }
 
             if(mysqli_connect_errno()){
                 echo "Erro ao tentar se conectar: ".mysqli_connect_error();
